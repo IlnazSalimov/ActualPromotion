80a2d896d41c209fc5e47ab29c588401d1cbe676
KulleR <kuller101@yandex.ru>
2015-01-20 16:32:52 +0300

    mor hags

--- DeVille/js/common.js
+++ DeVille/js/common.js
@@ -1,14 +1,26 @@
 ﻿$(document).ready(function () {
     var navElements = $('.main-menu .visible a');
     navElements.bind('click', function () {
+        console.log($(this));
         navElements.removeClass('active');
         $(this).addClass('active');
     });
 
-    var hiddenNav = $('.main-menu .hidden');
-    $('.main-menu .visible li:not(.button), .main-menu .hidden').hover(function () {
+    var hiddenNav = $('.main-menu .hidden'),
+        enrollForm = $('.enroll-form'),
+        onlineButton = $('.o-button');
+
+    $('.main-menu .visible li:not(.button-cont), .main-menu .hidden').hover(function () {
         hiddenNav.show();
     }, function () {
         hiddenNav.hide();
+    });
+
+    onlineButton.click(function () {
+        enrollForm.toggle("slide", { direction: "up" }, 1000);
+    });
+
+    enrollForm.children('.title').click(function () {
+        enrollForm.toggle("slide", { direction: "up" }, 1000);
     });
 });
